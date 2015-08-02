3ac0acc213b8c82dcdb410bdbc69f59875b3bb8a
iclown <gomezmendozacarlos@gmail.com>
2015-08-02 05:45:26 -0500

    se agregaron las funcionalidades de filtrado, ordenado y de los tabs principales

--- app.js
+++ app.js
@@ -6,24 +6,77 @@ wishlist.config(function ($routeProvider){
 		.when("/newitem",
 		{
 			templateUrl : "parcials/newitem.html",
-			controller: "usersController"
+			controller: "itemsController"
 		}).when("/edititem",
 		{
 			templateUrl : "parcials/newitem.html",
-			controller: "usersController"
+			controller: "itemsController"
 		}).when("/wishlist",
 		{
 			templateUrl : "parcials/tablist.html",
-			controller: "usersController"
+			controller: "itemsController"
 		}).when("/boughtlist",
 		{
 			templateUrl : "parcials/tablist.html",
-			controller: "usersController"
+			controller: "itemsController"
 		})
 });
 
-wishlist.controller('usersController', ['$scope', function($scope) {
-	
-
+wishlist.controller('itemsController', ['$scope','$rootScope',"Items", function($scope,$rootScope, prod) {	
+	$scope.ordenarPor = "-costo"
+	$scope.items = prod.list;
+	$rootScope.actualTab = "wishlist";
+	$scope.showItem = function(item) {
+		if($scope.actualTab === item ){ return true;};
+	} 
 }]);
+
+wishlist.factory("Items", function() {
+	var Items = {}
+
+	Items.list = [{
+		nombre: "Nombre del producto",
+		descripcion: "Descripcion del producto",
+		costo: 521,
+		fechaMod: 54654,
+		imagen : "http://a693.phobos.apple.com/us/r30/Purple/v4/c8/27/25/c8272541-648a-750b-7292-9fd82b6c3eb0/mzl.ugbojcbl.100x100-75.png",
+		status: "wishlist",
+		creado :  65454
+	},
+	{
+		nombre: "coca cola",
+		descripcion: "refresco de cola",
+		costo: 15,
+		fechaMod: 6541,
+		imagen : "http://a693.phobos.apple.com/us/r30/Purple/v4/c8/27/25/c8272541-648a-750b-7292-9fd82b6c3eb0/mzl.ugbojcbl.100x100-75.png",
+		status: "comprado",
+		creado :  65454
+	},
+	{
+		nombre: "xbox",
+		descripcion: "videoconsola",
+		costo: 14000,
+		fechaMod: 6541,
+		imagen : "http://a693.phobos.apple.com/us/r30/Purple/v4/c8/27/25/c8272541-648a-750b-7292-9fd82b6c3eb0/mzl.ugbojcbl.100x100-75.png",
+		status: "comprado",
+		creado :  65454
+	},
+	{
+		nombre: "ventilador",
+		descripcion: "maquina echa aigreee",
+		costo: 355,
+		fechaMod: 6541,
+		imagen : "http://a693.phobos.apple.com/us/r30/Purple/v4/c8/27/25/c8272541-648a-750b-7292-9fd82b6c3eb0/mzl.ugbojcbl.100x100-75.png",
+		status: "comprado",
+		creado :  65454
+	}
+	];
+
+	Items.add = function(message){
+    	Items.list.push({id: messages.list.length, text: message});
+  };
+
+
+	return Items;
+});
 
